0a0ecacd119fe756a4d2e88365ed1c0feffa459b
Miguel Salazar <yobebe@hotmail.com.ar>
2023-11-08 14:48:15 -0300

    tuve que crear otro repo para este proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 //import React from "react"; 
-import TodoList from "./componentes/TodoList/TodoList";
-import TodoItem from './componentes/TodoItem/TodoItem'
+import TodoList from "./componentes/TodoList/TodoList.jsx";
+import TodoItem from './componentes/TodoItem/TodoItem.jsx'
 
 const App = () => {
   return (
